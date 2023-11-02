86a4bc817e4301367070435f2ec7020198e84caf
zouhl <zouhl0924@gmail.com>
2023-11-02 20:27:20 +0800

    datasource的注解配置

--- springmvcdemo/src/main/java/com/homura/config/mybatis/MyBatisConfig.java
+++ springmvcdemo/src/main/java/com/homura/config/mybatis/MyBatisConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -20,7 +21,7 @@ import javax.sql.DataSource;
  */
 @Configuration
 @EnableTransactionManagement(proxyTargetClass = true, mode = AdviceMode.PROXY)
-
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @MapperScan(basePackages = "com.homura.dao", sqlSessionTemplateRef = "sqlSessionTemplate", sqlSessionFactoryRef = "sqlSessionFactory")
 public class MyBatisConfig {
 
